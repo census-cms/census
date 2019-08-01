a4965fb30f7596029ede5d52fa477e3c57cb58f5
[mas] Marc Scherer <m.scherer@imi.de>
2019-08-01 09:32:24 +0200

    DEV-8 add configuration

--- cms/settings/Configuration.php
+++ cms/settings/Configuration.php
@@ -6,9 +6,10 @@
  * package: census CMS
  */
 
-define('BASE_DIR', $baseDir);
-define('CONFIG_DIR', __DIR__);
-define('STORAGE_DIR', $baseDir . '/storage');
+define('BASE_DIR', $baseDir . DIRECTORY_SEPARATOR);
+define('CONFIG_DIR', __DIR__ . DIRECTORY_SEPARATOR);
+define('STORAGE_DIR', $baseDir . '/storage/');
+define('PLUGIN_DIR', STORAGE_DIR . '/plugins/');
 define('TEMPLATE_DIR', CONFIG_DIR . '/../templates/');
 
 return [
