0ffc353cc1e70a5956585c16b4c9564768a7ef28
Marc Scherer <m.scherer@imi.de>
2019-08-09 11:03:15 +0200

    DEV-2 update YAML parser wrapper

--- cms/classes/Core/Helper/Yaml.php
+++ cms/classes/Core/Helper/Yaml.php
@@ -10,30 +10,14 @@ namespace CENSUS\Core\Helper;
 class Yaml
 {
 	/**
-	 * @var \Symfony\Component\Yaml\Parser
-	 */
-	private $parser = null;
-
-	/**
-	 * @var \Symfony\Component\Yaml\Dumper
-	 */
-	private $dumper = null;
-
-	public function __construct ()
-	{
-		$this->parser = \CENSUS\Core\Helper\Utils::newInstance('\\Symfony\\Component\\Yaml\\Parser');
-		$this->dumper = \CENSUS\Core\Helper\Utils::newInstance('\\Symfony\\Component\\Yaml\\Dumper');
-	}
-
-	/**
 	 * Parses a YAML string to a PHP value
 	 *
 	 * @param string $value
 	 * @param int $flags
 	 */
-	public function parse(string $value, int $flags = 0)
+	public static function parse(string $value, int $flags = 0)
 	{
-		$this->parser->parse($value, $flags);
+		\Symfony\Component\Yaml\Parser::parse($value, $flags);
 	}
 
 	/**
@@ -42,9 +26,9 @@ class Yaml
 	 * @param string $filename
 	 * @param int $flags
 	 */
-	public function parseFile(string $filename, int $flags = 0)
+	public static function parseFile(string $filename, int $flags = 0)
 	{
-		$this->parser->parseFile($filename, $flags);
+		\Symfony\Component\Yaml\Parser::parseFile($filename, $flags);
 	}
 
 	/**
@@ -55,8 +39,20 @@ class Yaml
 	 * @param int $indent
 	 * @param int $flags
 	 */
-	private function dump($input, int $inline = 0, int $indent = 0, int $flags = 0)
+	public static function dump($input, int $inline = 0, int $indent = 0, int $flags = 0)
 	{
-		$this->dumper->dump($input, $inline, $indent, $flags);
+		self::getYamlDumper()->dump($input, $inline, $indent, $flags);
+	}
+
+	/**
+	 * Get the YAML Dumper
+	 *
+	 * @return \Symfony\Component\Yaml\Dumper
+	 */
+	private function getYamlDumper()
+	{
+		/** @var \Symfony\Component\Yaml\Dumper $yamlDumper */
+		$yamlDumper = \CENSUS\Core\Helper\Utils::newInstance('\\Symfony\\Component\\Yaml\\Dumper');
+		return $yamlDumper;
 	}
 }
